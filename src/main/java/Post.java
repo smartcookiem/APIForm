6dde1a02905d22b6046b5e1249e70866de84568e
Olga Cookie <angel_2008_year@mail.ru>
2023-04-03 22:52:11 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+
+
+}
